6ad0aaa547be7a7d65485b0ef883749bf334143d
Daniel Ik <codingeinstein@gmail.com>
2023-11-25 12:00:32 +0000

    add metrics.py

---
+++ src/metrics.py
@@ -0,0 +1,15 @@
+class Metric:
+    def __init__(self) -> None:
+        pass
+
+class MAP:
+    def __init__(self) -> None:
+        pass
+
+class DICE:
+    def __init__(self) -> None:
+        pass
+
+class IOU:
+    def __init__(self) -> None:
+        pass
