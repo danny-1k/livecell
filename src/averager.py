d85b5e14460d83c71c5fbadfeff63a4e8bca9f65
Daniel Ik <codingeinstein@gmail.com>
2023-11-26 19:51:38 +0000

    update averager.py

--- src/averager.py
+++ src/averager.py
@@ -1,11 +1,11 @@
 class Averager:
-    def __init__(self, gamma:float=.9):
+    def __init__(self, gamma:float=.9) -> None:
         self.i = 0
         self.value = 0
         self.gamma = gamma
         history = []
 
-    def __iadd__(self, value:float):
+    def __iadd__(self, value:float) -> None:
         self.i +=1
         self.value = self.gamma*value + (1-self.gamma)*self.value
         self.history.append(self.value)
