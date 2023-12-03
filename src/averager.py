2276a789e867c6da9e2df97d2f62b7cfb15b8a8b
Daniel Ik <codingeinstein@gmail.com>
2023-12-03 17:03:57 +0000

    fix minor bug

--- src/averager.py
+++ src/averager.py
@@ -3,9 +3,10 @@ class Averager:
         self.i = 0
         self.value = 0
         self.gamma = gamma
-        history = []
+        self.history = []
 
     def __iadd__(self, value:float) -> None:
         self.i +=1
         self.value = self.gamma*value + (1-self.gamma)*self.value
         self.history.append(self.value)
+        return self
